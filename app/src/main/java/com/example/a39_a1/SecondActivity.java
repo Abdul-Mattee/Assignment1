09a430f67f46585e8110b298f39e20ece9bcf8c3
Abdullah Qayyum <bsef19a039@pucit.edu.pk>
2022-05-22 16:48:22 +0500

    github repo button added

--- app/src/main/java/com/example/a39_a1/SecondActivity.java
+++ app/src/main/java/com/example/a39_a1/SecondActivity.java
@@ -13,6 +13,7 @@ public class SecondActivity extends AppCompatActivity {
         textView = findViewById(R.id.textContainer);
         Bundle valuesFromMain = getIntent().getExtras();
         int value = valuesFromMain.getInt("Number");
+        setTitle("Table of" + value);
         StringBuilder table = new StringBuilder();
         for (int i = 1;i<11;i++){
             String tableLine = String.format("%s  x  %s =\t%s\n", value, i,value*i); ;
